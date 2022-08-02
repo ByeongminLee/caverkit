f9aa3773686d567f28f2777cb92ec1a40b07f75c
bmlee531@gmail.com <bmlee531@gmail.com>
2022-08-02 21:45:13 +0900

    initial setting

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    'test-config',
+    'interfaces',
+    'jestGlobalMocks.ts',
+    '.module.ts',
+    '.misc.ts',
+    '.mock.ts',
+  ],
+};
